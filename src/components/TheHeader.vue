bfd92ba61348dcad927175a818fb9def487474af
Adrien Benete <adrbenete@gmail.com>
2021-12-14 16:36:40 +0900

    ajout de la section pour les projets

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,60 @@
+<template>
+     <header class="bg-dark">
+        <div class="container">
+          <div>
+            <nav class="navbar navbar-expand-lg navbar-dark">
+              <a class="navbar-brand" href="">
+                <i class="fas fa-laptop"></i>
+                Web developer in Fukuoka
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div id="navbarContent" class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                  <li class="nav-item active">
+                      
+                    <a class="nav-link" href=""><i class="fas fa-address-card"></i> About Me</a>
+                  </li>
+                  <li class="nav-item text-right active">
+                      
+                    <a class="nav-link" href=""><i class="fas fa-palette"></i> My works</a>
+                  </li>
+                  <li class="nav-item text-right active">
+                      
+                    <a class="nav-link" href=""><i class="fas fa-language"></i>日本語 </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>    
+    </header>
+</template>
+
+<script>
+
+
+export default {
+    name: 'TheHeader',
+}
+</script>
+
+<style lang="scss">
+header {
+    font-size: 130%;
+   
+    a{
+
+    color: #ffff;
+    padding-left: 1rem;
+   }
+   li{
+      padding-left: 2rem;
+   }
+  .navbar-expand-lg .navbar-collapse{
+      padding-left: 15rem;
+    justify-content: flex-end;
+  }  
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,148 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <TheHeader></TheHeader>
+   <img src="..\assets\coding-vs-programming-2.jpg" alt="picture of a web developer">
+   <div class="container">
+     <h2>About me</h2>
+     <p>Inspired by the creative aspect of Web Developing, I recently completed an online program with Openclassrooms. 
+       I am mainly focused on frontend but I also acquired some skills in backend developement with Node Js. 
+       I am really motivated and eager to learn, and most of all I enjoy coding</p>
+       <h2>My Skills</h2>
+       <img src="../assets/html.png" alt="logo html" class="logo">
+       <img src="../assets/css.png" alt="logo css" class="logo css">
+       <img src="../assets/js.png" alt="logo javascript" class="logo">
+       <img src="../assets/logo.png" alt="logo vue" class="logo">
+       <img src="../assets/sass.png" alt="logo sass" class="logo">
+       <img src="../assets/bootstrap.png" alt="logo bootstrap" class="logo">
+       <img src="../assets/mysql.png" alt="logo mysql" class="logo">
+       <img src="../assets/mongo.png" alt="logo mongodb" class="logo">
+       <img src="../assets/node.png" alt="logo node" class="logo">
+       
+
+       
+   </div>
+   <br>
+   <h2 class="text-center">My works</h2>
+   <br>
+   <div class="row">
+
+     
+     <div class="card ">
+  <img class="card-img-top reservia" src="..\assets\reservia.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Reservia</h5>
+    <p class="card-text">Turn Mock-ups Into a Web Page with HTML & CSS,  implementing a travel booking platform's website from scratch. The page is responsive</p>
+    <div class="pb-3">
+       
+    <img src="../assets/html.png" alt="logo html" class="logo">
+    <img src="../assets/css.png" alt="logo css" class="logo css">
+
+    </div>
+   
+    <a href="#" class="btn btn-primary">Check the code</a>
+    <a href="#" class="btn btn-primary ml-4">Check the webpage</a>
+    <img class="iphone" src="../assets/iphone.png" alt="">
+    
+  </div>
+</div>
+<div class="card">
+  <img class="card-img-top" src="..\assets\coding-vs-programming-2.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Creation of a </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+    
+    
+  </div>
+</div>
+
+
+
+   </div>
+   <footer>
+     Footer
+   </footer>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TheHeader from '@/components/TheHeader.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    "TheHeader": TheHeader
   }
 }
 </script>
+<style lang="scss">
+
+img{
+  width: 100%;
+  margin: auto;
+  max-width: 1700px;
+  object-fit: cover;
+}
+.logo{
+  width: 80px;
+  padding-right: 1rem;
+  transition: 100ms;
+
+  &:hover{
+    transform: scale(1.3);
+    transition: 300ms;
+    cursor: pointer;
+
+  }
+}
+.css{
+  width: 60px
+}
+
+.card{
+  width: 40rem;
+  position: relative;
+  border: solid 1px rgba(128, 128, 128, 0.678);
+  z-index: 1;
+  border-radius: 5px;
+  
+  .card-img-top {
+    height: 30rem;
+    object-fit: cover;
+    
+  }
+  .logo{
+  width: 60px;
+  padding-right: 1rem;
+  transition: 100ms;
+  }
+  .css{
+    width: 50px;
+  }
+  .reservia{
+      object-fit: cover;
+      object-position: left top;
+    }
+    .iphone{
+      height: 16rem;
+      width: 7.6rem;
+      position: absolute;
+      top: 270px;
+      right: 20px;
+      background-image: url("../assets/reserviaIphone1.png");
+      background-size: 8rem;
+  
+      border-radius: 15px;
+      
+     box-shadow: 5px 3px 5px rgba(63, 62, 62, 0.596);
+      
+
+      z-index: 3;
+
+    }
+}
+
+
+</style>
