eaa0baa8eb276c11dc1d68444152e6968e594c27
Adrien Benete <adrbenete@gmail.com>
2021-12-15 17:35:52 +0900

    ajout de l animation typewriting sur le titre de la page d acceuil

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,13 @@
      <TheHeader></TheHeader>
      <section class="banniere col-12 hero">
         <div class="hero-text">
-          <p>Adrien Benete</p>
-          <h1 class="pt-4 text-center">Front end developer in Fukuoka</h1>
+          <p>Adrien Bénété</p>
+          <h1 class="pt-4 text-center">
+            Web developer
+            <br>
+            <span class="typed-text">{{ typeValue }}</span>
+            <span class="cursor" :class="{'typing': typeStatus }">&nbsp;</span>
+           </h1>
         </div>
       </section>
    <div class="container">
@@ -119,8 +124,55 @@ export default {
   name: 'Home',
   components: {
     "TheHeader": TheHeader,
+  },
+  data:() =>{
+    return{
+      typeValue: '',
+      typeStatus: false,
+      typeArray: ['Front end', 'Full stack', 'Design'],
+      typingSpeed: 200,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      typeArrayIndex: 0,
+      charIndex: 0
 
-  
+    }
+  },
+  methods: {
+    typeText(){
+     if(this.charIndex < this.typeArray[this.typeArrayIndex].length){
+           if(!this.typeStatus)
+           this.typeStatus = true;
+           this.typeValue += this.typeArray[this.typeArrayIndex].charAt(this.charIndex)
+           this.charIndex += 1;
+
+           setTimeout(this.typeText, this.typingSpeed);
+     } else{
+              this.typeStatus = false;
+              setTimeout(this.eraseText, this.newTextDelay);
+     }
+    },
+    eraseText(){
+      if(this.charIndex > 0){
+        if(!this.typeStatus)
+        this.typeStatus = true;
+
+        this.typeValue = this.typeArray[this.typeArrayIndex].substring(0, this.charIndex -1 )
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed)
+      }
+      else{
+        this.typeStatus = false;
+        this.typeArrayIndex += 1;
+        if(this.typeArrayIndex >= this.typeArray.length)
+        this.typeArrayIndex = 0;
+
+        setTimeout(this.typeText, this.typingSpeed + 1000);
+      }
+    }
+  },
+  created(){
+    setTimeout(this.typeText, this.newTextDelay + 200)
   }
 }
 </script>
@@ -142,37 +194,41 @@ img{
     background-size: cover;
     height: 700px;
     width: 100%;
-    justify-content: center;
     position: relative;
 
     .hero-text{
-        position: absolute;
-        
-        
+        position: absolute;        
         align-items: center;
-        top: 20%;
-        left: 70%;
-        transform: translate(-50%, -50%);
+        top: 10%;
+        left: 60%;
     p{
-        font-size: 140%;
-        top: 20%;
-        left: 70%;
+      position: absolute;
+        font-size: 200%;
+        top: -20%;
+        left: 10%;
         
       
     }
-    
-    a{
-        background-color: white;
-        color: black;
-        text-align: center;
-        width: fit-content;
-        animation: slow-display ease-out 3000ms ;
-        transition: 700ms;
-        &:hover{
-            transform: scale(1.1);
-            transition: 700ms;
-        }
+    h1{
+      span.cursor{
+        display: inline-block;
+        margin-left: 3px;
+        width: 4px;
+        background-color: #fff;
+        animation: cursorBlink 1s infinite;
+        
+      }
+      span.cursor.typing{
+        animation: none;
+        
+      }
+      span.typed-text{
+       color: indianred;
+      }
+
     }
+    
+    
   }}
 .banner{
   height: 700px;
@@ -256,5 +312,11 @@ img{
     }
 }
 
+@keyframes cursorBlink{
+  49%{ background-color: #fff;}
+  50% {background-color: transparent;  }
+  99% {background-color: transparent;  }
+
+}
 
 </style>
